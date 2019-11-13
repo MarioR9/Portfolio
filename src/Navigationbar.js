c7cb62961f51301c4daee816f7725722593a78d5
MarioR9 <mariorodriguezan@gmail.com>
2019-11-13 14:24:54 -0500

    Resume added to be download

--- src/Navigationbar.js
+++ src/Navigationbar.js
@@ -16,12 +16,13 @@ export default class Navigationbar extends React.Component{
                 <Navbar.Text>
                 <Dropdown alignRight >
                 <Dropdown.Toggle variant="outline-success" id="dropdown-basic">
-                <Icon size='big' name='bars'/>
+                <Icon size='big' name='bars'/>  
                 </Dropdown.Toggle>
                 <Dropdown.Menu id="mySidenav" class="sidenav">
-                    <Dropdown.Item onClick={this.props.close} href="#about">About</Dropdown.Item>
-                    <Dropdown.Item onClick={this.props.close} href='#project'>Projects</Dropdown.Item>
-                    <Dropdown.Item onClick={this.props.close} href="#contact">Contact</Dropdown.Item>
+                    <Dropdown.Item onClick={this.props.close} href="#about"><Icon size='large' name='user outline'/>About</Dropdown.Item>
+                    <Dropdown.Item onClick={this.props.close} href='#project'><Icon size='large' name='clone outline'/>Projects</Dropdown.Item>
+                    <Dropdown.Item onClick={this.props.close} href="#contact"><Icon size='large' name='mail outline'/>Contact</Dropdown.Item>
+                    <Dropdown.Item href="/Resume.pdf" download="Mario_Rodriguez_Resume"><Icon size='large' name='file pdf outline'/>Resume</Dropdown.Item>
                 </Dropdown.Menu>
                 </Dropdown>
                 </Navbar.Text>
